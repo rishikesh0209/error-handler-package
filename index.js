df728323f4d6a6afb30cadef21070c3b541d2b49
Rishikesh Shetty <rishikesh.shetty@zoho.com>
2023-11-23 00:13:04 +0530

    package changes

--- index.js
+++ index.js
@@ -5,41 +5,45 @@ const errorCodeParser = (err) => {
         code: 500,
         message: 'Something went wrong !!'
     };
-    let errorMessage = _.get(err, 'body.error', null);
-    let errorCase = "UNKNOWN";
-    let errArray = [];
-    if (_.get(err, 'raw.message', null)) {
-        errArray = err.raw.message.split(" ");
-    } else if (err.message) {
-        errArray = err.message.split(" ");
+    try {
+        let errorMessage = _.get(err, 'body.error', null);
+        let errorCase = "UNKNOWN";
+        let errArray = [];
+        if (_.get(err, 'raw.message', null)) {
+            errArray = err.raw.message.split(" ");
+        } else if (err.message) {
+            errArray = err.message.split(" ");
+        }
+        errorCase = errArray.length > 0 ? errArray[0] : [];
+        if (errArray.length) {
+            polishedError.message = `${errArray.slice(1).join(" ")}`;
+        }
+        switch (errorCase) {
+            case 'BAD_REQUEST':
+            case 'BAD_REQUEST_TO_IGNORE':
+                polishedError.code = 400;
+                break;
+            case 'NOT_FOUND':
+                polishedError.code = 404;
+                break;
+            case 'FORBIDDEN':
+                polishedError.code = 403;
+                break;
+            case 'CONFLICT':
+                polishedError.code = 409;
+                break;
+            case 'UNPROCESSABLE_CONTENT':
+                polishedError.code = 422;
+                break;
+            default:
+                polishedError.code = 500;
+                polishedError.message = errorMessage ? errorMessage : polishedError.message;
+                break;
+        }
+        return polishedError;
+    } catch (err) {
+        return polishedError;
     }
-    errCase = errArray.length > 0 ? errArray[0] : [];
-    if (errArray[1].length && typeof errArray[1] == "string") {
-        polishedError.message = `${errArray[1].slice(1).join(" ")}`;
-    }
-    switch (errorCase) {
-        case 'BAD_REQUEST':
-        case 'BAD_REQUEST_TO_IGNORE':
-            polishedError.code = 400;
-            break;
-        case 'NOT_FOUND':
-            polishedError.code = 404;
-            break;
-        case 'FORBIDDEN':
-            polishedError.code = 403;
-            break;
-        case 'CONFLICT':
-            polishedError.code = 409;
-            break;
-        case 'UNPROCESSIBLE CONTENT':
-            polishedError.code = 422;
-            break;
-        default:
-            polishedError.code = 500;
-            polishedError.message = errorMessage ? errorMessage : polishedError.message;
-            break;
-    }
-    return polishedError;
 }
 
 module.exports = errorCodeParser;
